20e1cdaabce3c4919c0484bc5e7a395f4a24c9f0
victo <victo@LAPTOP-7NAMSI81>
2021-04-21 10:52:30 -0500

    Proyecto prueba Github.

---
+++ src/prueba_git/Prueba_git.java
@@ -0,0 +1,11 @@
+package prueba_git;
+
+public class Prueba_git {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Esto es una prueba para github");
+        
+    }
+    
+}
